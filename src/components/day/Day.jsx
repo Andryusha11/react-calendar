2bdc4bea7b4308baeda43b6675a784d550bdfa86
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-23 16:02:30 +0200

    add red line

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Hour from '../hour/Hour';
 import './day.scss';
 
 const Day = ({ dataDay, dayEvents }) => {
+  const currentDay = dataDay === new Date().getDate();
   const hours = Array(24)
     .fill()
     .map((val, index) => index);
 
-  return (
-    <div className="calendar__day" data-day={dataDay}>
-      {hours.map((hour) => {
-        //getting all events from the day we will render
-        const hourEvents = dayEvents.filter(
-          (event) => event.dateFrom.getHours() === hour
-        );
+  const [styleForRedLine, setStyleForRedLine] = useState({
+    top: new Date().getHours() * 60 + new Date().getMinutes(),
+  });
 
-        return (
-          <Hour key={dataDay + hour} dataHour={hour} hourEvents={hourEvents} />
-        );
-      })}
-    </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setStyleForRedLine({
+        top: new Date().getHours() * 60 + new Date().getMinutes(),
+      });
+    }, 60000);
+  });
+
+  return (
+    <>
+      <div className="calendar__day" data-day={dataDay}>
+        {hours.map((hour) => {
+          //getting all events from the day we will render
+          const hourEvents = dayEvents.filter(
+            (event) => event.dateFrom.getHours() === hour
+          );
+
+          return (
+            <>
+              <Hour
+                key={dataDay + hour}
+                dataHour={hour}
+                hourEvents={hourEvents}
+              />
+            </>
+          );
+        })}
+        {currentDay ? (
+          <div
+            key={styleForRedLine.top}
+            style={styleForRedLine}
+            className="red-line"
+          ></div>
+        ) : null}
+      </div>
+    </>
   );
 };
 
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,12 +3,29 @@ import { days } from '../../utils/dateUtils.js';
 import './navigation.scss';
 
 const Navigation = ({ weekDates }) => {
+  const styleForCurrentDay = (dayDate) =>
+    dayDate === new Date().getDate()
+      ? {
+          backgroundColor: '#1A73E8',
+          borderRadius: '50%',
+          color: '#FFFFFF',
+          width: '36px',
+          height: '36px',
+          textAlign: 'center',
+          lineHeight: '36px',
+        }
+      : null;
   return (
     <header className="calendar__header">
       {weekDates.map((dayDate) => (
         <div key={dayDate.getDay()} className="calendar__day-label day-label">
           <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
+          <span
+            style={styleForCurrentDay(dayDate.getDate())}
+            className="day-label__day-number"
+          >
+            {dayDate.getDate()}
+          </span>
         </div>
       ))}
     </header>
